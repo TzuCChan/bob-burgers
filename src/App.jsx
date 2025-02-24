290a94ba7e4fec879f48a9f3c01b938878f99453
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2025-02-24 12:15:12 -0500

    app.jsx function app()

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+
+function App() {
+  const [width, setWidth] = useState('0%');
+  const [image, setImage] = useState(
+    'https://bobsburgers-api.herokuapp.com/images/characters/319.jpg'
+  );
+}
